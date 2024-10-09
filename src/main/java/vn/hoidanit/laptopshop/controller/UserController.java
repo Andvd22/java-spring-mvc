d7a0a854896a976ce0fed08726fb7c6df09b27d2
Andvd22 <dovanan774@gmail.com>
2024-10-10 01:20:43 +0700

    studyv49

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -2,50 +2,38 @@ package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.ui.Model;
 import vn.hoidanit.laptopshop.service.UserService;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
 
 @Controller
 public class UserController {
-    
-    private UserService userService;
 
-    
+    private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("anric", test);
+        model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
 }
 
-
-
 // @RestController
 // public class UserController {
-    
-//     private UserService userService;
 
-    
+// private UserService userService;
 
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
 
-
-
-//     @GetMapping("")
-//     public String getHomePage() {
-//         return this.userService.handleHello();
-//     }
+// @GetMapping("")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
 // }
--- src/main/webapp/WEB-INF/view/hello.jsp
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -1,3 +1,8 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+
+
 <html lang="en">
     <head>
         <meta charset="UTF-8" />
@@ -5,6 +10,8 @@
         <title>Document</title>
     </head>
     <body>
+        <h1>${anric}</h1>
+        <h2>${hoidanit}</h2>
         hello from jsp
     </body>
 </html>
