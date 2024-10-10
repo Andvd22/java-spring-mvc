9c7e54aec686685241c67b0a872dce0bc18af8b9
Andvd22 <dovanan774@gmail.com>
2024-10-10 13:55:31 +0700

    studyv67

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,12 +7,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -20,8 +21,7 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("anric", test);
+        model.addAttribute("anric", "test");
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
@@ -34,8 +34,8 @@ public class UserController {
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User dovanan) {
-        String test = this.userService.handleHello();
         System.out.println("run here" + dovanan);
+        this.userService.handelSaveUser(dovanan);
         return "hello";
     }
 
